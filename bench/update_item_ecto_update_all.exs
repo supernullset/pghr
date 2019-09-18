1024b16ef750911b5a24a0b7d6f68faceb6bc600
supernullset <sean.j.williamson@gmail.com>
2019-09-18 11:54:21 -0700

    do fast inserts

--- bench/update_item_ecto_update_all.exs
+++ bench/update_item_ecto_update_all.exs
@@ -6,8 +6,6 @@ IO.puts("Deleting all existing items ...")
 
 Repo.delete_all(Item)
 
-
-
 total_items = 100_000
 runtime = 120
 warmup = 30
@@ -15,20 +13,24 @@ parallel = 5
 
 IO.puts("Creating #{total_items} new items ...")
 
-item_ids =
-  Enum.map(1..total_items, fn id ->
-    {:ok, %{id: id}} =
-      Repo.insert(%Item{
-        mumble1: "mumble",
-        mumble2: "Mumble-#{id}",
-        mumble3: "Moar Mumble #{id}"
-      })
+seed_database_sql = """
+DO $$
+BEGIN
+    FOR id IN 1..#{total_items} LOOP
+        INSERT INTO items (mumble1, mumble2, mumble3)
+        VALUES ('mumble', 'Mumble-' || id, 'Moar Mumble ' || id);
+    END LOOP;
+END; $$
+"""
+Ecto.Adapters.SQL.query!(Repo, "ALTER SEQUENCE items_id_seq RESTART WITH 1;", []);
+Ecto.Adapters.SQL.query!(Repo, seed_database_sql, []);
 
-    id
-  end)
+item_ids = 1..total_items
+
+item_count = Repo.aggregate(Item, :count, :id)
+|> IO.inspect(label: "item count")
 
 IO.puts("Starting test ...")
-
 ParallelBench.run(
   fn ->
     random_item_id = Enum.random(item_ids)
