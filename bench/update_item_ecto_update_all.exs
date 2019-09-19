99870544ec5d14993fc759321c93b55019032dd7
supernullset <sean.j.williamson@gmail.com>
2019-09-19 12:19:31 -0700

    merge conflicts

--- bench/update_item_ecto_update_all.exs
+++ bench/update_item_ecto_update_all.exs
@@ -8,8 +8,7 @@ Repo.delete_all(Item)
 
 total_items = 100_000
 runtime = 120
-warmup = 30
-parallel = 1
+parallel = 10
 
 IO.puts("Creating #{total_items} new items ...")
 
@@ -22,48 +21,24 @@ BEGIN
     END LOOP;
 END; $$
 """
-Ecto.Adapters.SQL.query!(Repo, "ALTER SEQUENCE items_id_seq RESTART WITH 1;", []);
-Ecto.Adapters.SQL.query!(Repo, seed_database_sql, []);
 
-item_ids = 1..total_items
+Ecto.Adapters.SQL.query!(Repo, seed_database_sql, []);
 
 item_count = Repo.aggregate(Item, :count, :id)
 |> IO.inspect(label: "item count")
 
-#defmodule UpdateServer do
-#  use GenServer
-#
-#  def start_link(item_size) do
-#    GenServer.start_link(__MODULE__, [item_size])
-#  end
-#
-#  def init(item_size) do
-#    Process.send_after(:run)
-#    {:ok, {1, item_size}}
-#  end
-#
-#  def handle_call(:run, {current_id, total_id}) do
-#    {:ok, _} =
-#      %Item{id: current_id}
-#      |> Ecto.Changeset.change(%{mumble3: "New Mumble #{random}"})
-#      |> Repo.update()
-#    Process.send(:run)
-#    {:ok, {current_id + 1, total_id}}
-#  end
-#end
-
 IO.puts("Starting test ...")
 ParallelBench.run(
   fn ->
-    random_item_id = Enum.random(item_ids)
-    random = :rand.uniform(100_000_000_000_000)
+    random_item_id = :rand.uniform(total_items)
+    random = :rand.uniform(total_items)
 
     {1, _} =
       from(i in Item, where: i.id == ^random_item_id)
       |> Repo.update_all(set: [mumble3: "New Mumble #{random}"])
   end,
-  parallel: 10,
-  duration: 10
+  parallel: parallel,
+  duration: runtime
 )
 
 item_count = Repo.aggregate(Item, :count, :id)
